deee540a12481ccfb4ae30756a27560f6c4f7beb
beni-pane <benicio.panettieri@gmail.com>
2026-01-04 17:10:00 +0100

    prueba carga de juego, por error MIME

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/chess/',
+  base: '/chess/', // Esto asegura que los assets se busquen en /chess/assets/
   build: {
     outDir: 'dist',
+    assetsDir: 'assets',
   }
 });
